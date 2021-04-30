733b8714b362d1cb07ae72467f451164bd19f718
Anwarul Islam <anwaarulislaam@gmail.com>
2021-04-30 17:06:25 +0600

    database

--- crawler/crawl.js
+++ crawler/crawl.js
@@ -3,50 +3,69 @@ const xl = require('excel4node')
 const Brand = require('../model/Brand')
 
 let pharmaAll = [71, 136, 1, 25, 16, 119, 155, 57, 50, 113, 110, 70, 2, 68, 154, 127, 21, 104, 131, 105, 116, 117, 165, 42, 82, 161, 171, 61, 143]
+// let pharmaAll = [71, 136, 1, 143]
 
-const headingColumnNames = [
-    "Sl", "Supplier Name", "Product Name", "Product Model", "Category Name", "Sale Price", "Supplier Price",
-]
+function makeSinglePharma(pharma_id) {
 
-const wb = new xl.Workbook();
-const ws = wb.addWorksheet('data');
+    const headingColumnNames = [
+        "Sl", "Supplier Name", "Product Name", "Product Model", "Category Name", "Sale Price", "Supplier Price",
+    ]
 
-let headingColumnIndex = 1;
-headingColumnNames.forEach(heading => {
-    ws.cell(1, headingColumnIndex++)
-        .string(heading)
-});
+    const wb = new xl.Workbook();
+    const ws = wb.addWorksheet('data');
 
-Brand.findAll({
-    attributes: ['title', 'pharma_name', 'prices', 'dosage_form'],
-    where: { pharma_id: pharmaAll },
-    limit: 10
-}).then((data) => {
-
-    let rowIndex = 2;
-
-    let mapped = data.map((d, i) => {
-        return {
-            "SL": `${i+1}`,
-            "Supplier Name": d.pharma_name,
-            "Product Name": d.title,
-            "Product Model": d.dosage_form,
-            "Category Name": d.pharma_name,
-            "Sale Price": d.prices,
-            "Supplier Price": d.prices
-        }
-    })
-
-    mapped.forEach(record => {
-        let columnIndex = 1;
-        Object.keys(record).forEach(columnName => {
-            ws.cell(rowIndex, columnIndex++)
-                .string(record[columnName])
-        });
-        rowIndex++;
+    let headingColumnIndex = 1;
+    headingColumnNames.forEach(heading => {
+        ws.cell(1, headingColumnIndex++)
+            .string(heading)
     });
 
-    wb.write('filename.xlsx');
+    Brand.findAll({
+        attributes: ['title', 'pharma_name', 'prices', 'dosage_form', 'strength'],
+        where: { pharma_id: pharma_id },
+        // limit: 1000
+    }).then((data) => {
 
-    console.log(JSON.stringify(data))
+        let rowIndex = 2;
+
+        let mapped = data.map((d, i) => {
+
+            let prices = d.prices ? JSON.parse(d.prices) : null
+
+            let price = null;
+
+            if (prices && prices.length > 0) {
+                price = prices[0].price
+            }
+
+            return {
+                "SL": `${i + 1}`,
+                "Supplier Name": d.pharma_name,
+                "Product Name": `${d.title} - ${d.strength} - ${d.dosage_form}`,
+                "Product Model": d.dosage_form,
+                "Category Name": d.pharma_name,
+                "Sale Price": price,
+                "Supplier Price": `${price - ((price / 100) * 12)}`
+            }
+        })
+
+        mapped.forEach(record => {
+            let columnIndex = 1;
+            Object.keys(record).forEach(columnName => {
+                ws.cell(rowIndex, columnIndex++)
+                    .string(record[columnName])
+            });
+            rowIndex++;
+        });
+
+        wb.write(`all_pharma/${(mapped[0]['Supplier Name']).replace('.', '')}.xlsx`);
+        console.log('done')
+    })
+}
+
+
+// makeSinglePharma([5])
+
+pharmaAll.forEach(x => {
+    makeSinglePharma([x])
 })
